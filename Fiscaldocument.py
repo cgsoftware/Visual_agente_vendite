0ebae31b2f2fb899405bde28c7c38bff2723e3bc
Giu Skipper <g.dalo@cgsoftware.it>
2012-05-10 09:57:32 +0200

    Corretto la return della on_change_partner

--- Fiscaldocument.py
+++ Fiscaldocument.py
@@ -19,39 +19,11 @@ class FiscalDocHeader(osv.osv):
                }
    def onchange_partner_id(self, cr, uid, ids, part, context):
         res = super(FiscalDocHeader, self).onchange_partner_id(cr, uid, ids, part, context)
-        if not part:
-            return {'value': {'partner_indfat_id': False, 'partner_indcons_id': False}}
-        addr = self.pool.get('res.partner').address_get(cr, uid, [part], ['delivery', 'invoice', 'contact'])
-        part = self.pool.get('res.partner').browse(cr, uid, part)
-        pricelist = part.property_product_pricelist and part.property_product_pricelist.id or False
-        pagamento_id = part.property_payment_term and part.property_payment_term.id or False
-#       fiscal_position = part.property_account_position and part.property_account_position.id or False
-        agente = part.user_id and part.user_id.id or uid
-        if part.bank_ids:
-            banca_cliente = part.bank_ids[0].bank.id
-        else:
-            banca_cliente = False
-
-        val = {
-            'partner_indfat_id': addr['invoice'],
-            'partner_indcons_id': addr['delivery'],
-            'pagamento_id':pagamento_id,
-            'sconto_pagamento':part.property_payment_term.sconto,
-            'agente':agente,
-            'str_sconto_partner':part.str_sconto_partner,
-            'sconto_partner':part.sconto_partner,
-            'spedizione':part.spedizione.id,
-            'vettore':part.property_delivery_carrier.id,
-            'porto_id':part.carriage_condition_id.id,
-            'aspetto_esteriore_id':part.goods_description_id.id,
-            'banca_patner':banca_cliente,
-            'agente2':part.agent_id.id
-            
-        }
-        if pricelist:
-            val['listino_id'] = pricelist
-
-        warning = {}
+        val = res.get('value', False)
+        warning = res.get('warning', False)
+        #import pdb;pdb.set_trace()
+        if val and part.agent_id:
+            val['agente2']=part.agent_id.id
             
         
             
